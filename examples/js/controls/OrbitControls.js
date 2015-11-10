7a8b6c29882777ac0eee193c507d1b2ee92236b0
kriffe <kristofer.skyttner@skymaker.se>
2015-11-10 18:06:46 +0100

    Direction fix for zoom to mouse addition

--- examples/js/controls/OrbitControls.js
+++ examples/js/controls/OrbitControls.js
@@ -616,10 +616,9 @@
                         
                         //Include pan towards target if enabled
                         if (scope.enableZoomToMouse){
-                            var dir = (delta > 0 ? -1 : 1);
                             var deltaPanX = event.clientX - (scope.domElement.offsetLeft + scope.domElement.clientWidth/2);
                             var deltaPanY = event.clientY - (scope.domElement.offsetTop + scope.domElement.clientHeight/2);
-                            pan( dir*deltaPanX/10,dir*deltaPanY/10);
+                            pan( -deltaPanX/10,-deltaPanY/10);
                         }
                         
 
